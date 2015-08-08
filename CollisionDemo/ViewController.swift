17e165495d3d6a00afa1a08c5d8b9fdb8ce7c123
Jeff Barg <jeffbarg@wharton.upenn.edu>
2015-08-08 12:59:29 -0700

    Initial Commit

---
+++ CollisionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollisionDemo
+//
+//  Created by Jeff Barg on 8/8/15.
+//  Copyright © 2015 Jeff Barg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

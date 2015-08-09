75d25accf4c2f35bd12886908e0186b57f991305
Jeff Barg <jeffbarg@wharton.upenn.edu>
2015-08-08 17:52:15 -0700

    First Commit

--- CollisionDemo/ViewController.swift
+++ CollisionDemo/ViewController.swift
@@ -7,12 +7,76 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    var itemViews : [UIView] = []
+    
+    let motionManager = CMMotionManager()
+    let motionQueue = NSOperationQueue()
+    
+    var animator : UIDynamicAnimator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for _ in 1...10 {
+            
+            let width = arc4random() % 75 + 25
+            let height = arc4random() % 75 + 25
+            
+            let itemView = UIView(
+                frame: CGRectMake(
+                    CGFloat(self.view.frame.size.width / 2.0),
+                    CGFloat(self.view.frame.size.height / 2.0),
+                    CGFloat(width),
+                    CGFloat(height)
+                )
+            )
+            
+            itemView.backgroundColor = UIColor.randomFlatColor()
+            itemViews.append(itemView)
+            view.addSubview(itemView)
+        }
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        
+        let gravityBehavior = UIGravityBehavior(items: itemViews)
+
+        motionManager.startDeviceMotionUpdatesToQueue(self.motionQueue, withHandler: {motion, error in
+            if error != nil {
+                return
+            }
+            
+            guard let gravity = motion?.gravity else {
+                return
+            }
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                gravityBehavior.gravityDirection = CGVectorMake(
+                    CGFloat(gravity.x),
+                    CGFloat(-gravity.y)
+                )
+            }
+        })
+        
+        let collisionBehavior = UICollisionBehavior(items: itemViews)
+
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        
+        animator?.addBehavior(gravityBehavior)
+        animator?.addBehavior(collisionBehavior)
+        
+        for item in itemViews {
+            let dynamicBehavior = UIDynamicItemBehavior(items: [item])
+            
+            let area = item.frame.size.width * item.frame.size.height
+            dynamicBehavior.elasticity = area / (100 * 100)
+            dynamicBehavior.friction = 0.1
+            
+            animator?.addBehavior(dynamicBehavior)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +86,3 @@ class ViewController: UIViewController {
 
 
 }
-
